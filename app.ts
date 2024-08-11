c2637aa145d85aa30f573bb3e5624647f64db6a3
yuxnzs <yuchengliu0429@gmail.com>
2024-08-12 00:55:12 +0800

    Implement route and functions for restaurant's detail page data

--- app.ts
+++ app.ts
@@ -1,10 +1,40 @@
 import express from "express";
-import { getAllRestaurants } from "./restaurant";
+import {
+  getAllRestaurants,
+  getRestaurantById,
+  getRestaurantMenuItemCount,
+} from "./restaurant";
+import { getNumberOfMeals } from "./meal-db-service";
 
 const app = express();
 
+// Return data for the home page
 app.get("/restaurants", (req, res) => {
   res.send(getAllRestaurants());
+});
+
+// Return data for a restaurant's detail page
+app.get("/restaurants/:category/:id", async (req, res) => {
+  const { category, id } = req.params;
+
+  // Convert string id to number
+  let numberId = parseInt(id, 10);
+  if (isNaN(numberId)) {
+    res.status(400).send("Invalid ID");
+    return;
+  }
+
+  const menuItemCount = getRestaurantMenuItemCount(numberId);
+  if (menuItemCount === undefined) {
+    res.status(400).send("Error getting menu items");
+    return;
+  }
+
+  // Combine restaurant info and meals data from TheMealDB
+  const restaurant = getRestaurantById(numberId);
+  const meals = await getNumberOfMeals(category, menuItemCount);
+
+  res.json({ restaurant, meals });
 });
 
 app.listen(3000, () => {
--- meal-db-service.ts
+++ meal-db-service.ts
@@ -1,6 +1,17 @@
 import axios from "axios";
 
-async function getMealDB(category: string) {
+interface MealDBResponse {
+  meals: Meal[];
+}
+
+interface Meal {
+  strMeal: string;
+  strMealThumb: string;
+  idMeal: string;
+}
+
+// Get meals from API
+async function getMealDB(category: string): Promise<MealDBResponse> {
   const response = await axios.get(
     `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`
   );
@@ -8,10 +19,35 @@ async function getMealDB(category: string) {
   return response.data;
 }
 
-async function getNumberOfMeals(category: string, from: number, to: number) {
+// Get certain category and certain number of random meals from API response
+async function getNumberOfMeals(category: string, menuItems: number) {
   const meals = await getMealDB(category);
+  const randomMeals = getRandomElements(meals.meals, menuItems);
 
-  return meals.meals.slice(from, to);
+  return randomMeals.map((meal: Meal) => {
+    return {
+      ...meal,
+      price: getMealPrice(meal.idMeal),
+    };
+  });
 }
 
-export default getNumberOfMeals;
+// Convert meal id to price, e.g. 52772 => 17.72
+function getMealPrice(mealId: string): number {
+  const numberId = parseInt(mealId, 10);
+  if (isNaN(numberId)) {
+    return 0;
+  }
+
+  const lastFourDigits = numberId % 10000; // 52772 to 2772
+  const price = 10 + (lastFourDigits % 1000) / 100; // Convert to xx.xx format, e.g. 12.72
+  return parseFloat(price.toFixed(2)); // Round to two decimals
+}
+
+// Get random elements from an array
+function getRandomElements<T>(array: T[], count: number): T[] {
+  const shuffled = array.sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, count);
+}
+
+export { getNumberOfMeals };
